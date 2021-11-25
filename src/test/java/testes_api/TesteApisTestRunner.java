9da60a4ce5683f2b07aa6d796755cae99be9e543
GabrielHS <gabrielhenri0012@gmail.com>
2021-11-25 20:55:49 -0300

    Add: suite de teste espeficico para ficar trocando tag e suite de teste para todos os testes

---
+++ src/test/java/testes_api/TesteApisTestRunner.java
@@ -0,0 +1,19 @@
+package testes_api;
+
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class TesteApisTestRunner {
+
+    @Test
+    void testParallel() {
+        Results results = Runner.path("classpath:testes_api")
+                .tags("~@ignore")
+                //.outputCucumberJson(true)
+                .parallel(5);
+        assertEquals(0, results.getFailCount(), results.getErrorMessages());
+    }
+
+}
